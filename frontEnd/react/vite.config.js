c34ccc5060414b443b8467780a346b2018daa6da
CARMEL <ndacarmel6@gmail.com>
2025-07-18 15:18:10 +0000

    merci ok

--- frontEnd/react/vite.config.js
+++ frontEnd/react/vite.config.js
@@ -4,20 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  
-  server: {
-    headers: {
-      "Content-Security-Policy": "default-src 'self'; connect-src 'self' https://applivrerecommendationfullstack.onrender.com;"
-    }
-  },
-  
-  // Pour la version de production (build)
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  }
+  plugins: [react() ,    tailwindcss(),
+  ]
 })
