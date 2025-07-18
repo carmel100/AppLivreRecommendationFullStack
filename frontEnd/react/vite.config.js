e9d5b6b7e6821ac9c1bdd6e90995f482ed77d65c
CARMEL <ndacarmel6@gmail.com>
2025-07-18 14:10:38 +0000

    punch1

--- frontEnd/react/vite.config.js
+++ frontEnd/react/vite.config.js
@@ -4,6 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,    tailwindcss(),
-  ]
+  plugins: [react(), tailwindcss()],
+  
+  server: {
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; connect-src 'self' https://ton-backend-domaine.com;"
+    }
+  },
+  
+  // Pour la version de production (build)
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  }
 })
