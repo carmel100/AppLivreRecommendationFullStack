c597301edf84ae3b567958db34b481ceccb81f18
CARMEL <ndacarmel6@gmail.com>
2025-07-21 15:10:44 +0000

    opinion

--- backEnd/protected.js
+++ backEnd/protected.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const verifierToken = require('./verifyToken');
 
-router.get('/accueil', verifierToken, (req, res) => {
+router.get('/protected', verifierToken, (req, res) => {
   res.status(200).json({ message: `Bienvenue ${req.user.email}` });
 });
 
